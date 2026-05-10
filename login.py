29fb626b89f445689d18bf39fc8ba5239c23c5cc
aizensosuke <clevencastillo10@gmail.com>
2026-05-10 10:40:04 +0800

    added main entry point

--- login.py
+++ login.py
@@ -1,12 +1,7 @@
 from tkinter import *
 import ttkbootstrap
 
-window = Tk()
+# initialize window
 
-
-def init_login():
-    window.mainloop()
-
-
-if __name__ == '__main__':
-    init_login()
+def open_login(window):
+    window = Toplevel("Login")
---
+++ main.py
@@ -0,0 +1,9 @@
+from login import open_login
+from tkinter import Tk
+
+window = Tk()
+window.withdraw()
+
+open_login(window)
+
+window.mainloop()
